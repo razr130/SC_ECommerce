e251ea3fb807271c5e60ca045a63d95c92e6ec39
JV Fernando <jovfernando@yahoo.com>
2019-04-23 11:08:36 +0700

    adding login page

--- app/src/main/java/com/example/sc_ecommerce/MainActivity.java
+++ app/src/main/java/com/example/sc_ecommerce/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.sc_ecommerce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_regis, btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_regis = (Button) findViewById(R.id.btn_register);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent  = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
